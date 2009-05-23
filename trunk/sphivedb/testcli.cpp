66ce97bc51e0389f9d1c69fde4c7ab149b1965c1
stephen.nil <stephen.nil@6ff03308-3f09-11de-90e1-f9f1724fc9cc>
2009-05-23 13:55:36 +0000

    add more function for tetcli

--- trunk/sphivedb/testcli.cpp
+++ trunk/sphivedb/testcli.cpp
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "spnetkit/spnksocket.hpp"
 #include "spnetkit/spnklog.hpp"
@@ -20,10 +21,14 @@
 
 void showUsage( const char * program )
 {
-	printf( "\n\n%s [-h host] [-p port] [-o] [-v]\n", program );
+	printf( "\n\n%s [-h host] [-p port] [-i dbfile] [-u user] [-n dbname] [-s sql] [-o] [-v]\n", program );
 
 	printf( "\t-h http host\n" );
 	printf( "\t-p http port\n" );
+	printf( "\t-i dbfile\n" );
+	printf( "\t-u user\n" );
+	printf( "\t-n dbname\n" );
+	printf( "\t-s sql script\n" );
 	printf( "\t-o log socket io\n" );
 	printf( "\t-v show this usage\n" );
 	printf( "\n\n" );
@@ -62,36 +67,53 @@ int main( int argc, char * argv[] )
 	SP_NKLog::setLogLevel( LOG_DEBUG );
 
 	char * host = NULL, * port = NULL;
+	char * dbfile = NULL, * user = NULL, * dbname = NULL, * sql = NULL;
 
 	extern char *optarg ;
 	int c ;
 
-	while( ( c = getopt( argc, argv, "h:p:ov" ) ) != EOF ) {
+	while( ( c = getopt( argc, argv, "h:p:i:u:n:s:ov" ) ) != EOF ) {
 		switch ( c ) {
 			case 'h' : host = optarg; break;
 			case 'p' : port = optarg; break;
+			case 'i' : dbfile = optarg; break;
+			case 'u' : user = optarg; break;
+			case 'n' : dbname = optarg; break;
+			case 's' : sql = optarg; break;
 			case 'o' : SP_NKSocket::setLogSocketDefault( 1 ); break;
 			case 'v' :
 			default: showUsage( argv[ 0 ] ); break;
 		}
 	}
 
-	if( NULL == host || NULL == host ) {
-		printf( "Please specify host and port!\n" );
+	if( NULL == host || NULL == host || NULL == dbfile
+			|| NULL == user || NULL == dbname || NULL == sql ) {
 		showUsage( argv[ 0 ] );
 	}
 
-	SP_NKStringList sql;
-	sql.append( "insert into addrbook values ( 1, \"abc\" )" );
-	sql.append( "select rowid, * from addrbook" );
+	SP_NKStringList sqlList;
+	{
+		sql = strdup( sql );
+
+		for( char * pos = sql; NULL != pos && '\0' != *pos; ) {
+			char * next = strchr( pos, ';' );
+			if( NULL != next ) *next = '\0';
+
+			for( ; isspace( *pos ); ) pos++;
+
+			if( '\0' != *pos ) sqlList.append( pos );
+
+			pos = next ? next + 1 : NULL;
+		}
+
+		free( sql );
+	}
 
 	SP_NKTcpSocket socket( host, atoi( port ) );
 
 	SP_HiveDBProtocol protocol( &socket, 0 );
 
-	int dbfile = 19;
-
-	SP_HiveRespObject * resp = protocol.execute( dbfile, "stephen", "addrbook", &sql );
+	SP_HiveRespObject * resp = protocol.execute( atoi( dbfile ), user, dbname, &sqlList );
 
 	if( NULL != resp ) {
 		if( 0 == resp->getErrorCode() ) {
